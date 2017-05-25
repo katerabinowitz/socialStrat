dfb4140e5a6cf4e8158dcb69c773044b8a2ebb5c
katerabinowitz <cathryn.rabinowitz@gmail.com>
2017-05-25 16:01:47 -0400

    refill

--- data/iconDataCreation.R
+++ data/iconDataCreation.R
@@ -1,5 +1,4 @@
 library(dplyr)
-library(reshape2)
 
 # create dataset structure for low end
 # create dataset structure for low end
@@ -36,8 +35,8 @@ iconsHi<-read.csv("iconsFilledHi.csv", stringsAsFactors = FALSE)
 iconsLoSub <- iconsLo %>% subset(!(income %in% c("inc200300","inc300400","inc400+")))
 
 icons <- rbind(iconsLoSub, iconsHi) %>%
-         mutate(income = factor(income, levels = c("inc400+", "inc375400", "inc350375", "inc325350", "inc300325", "inc275300", "inc250275", "inc225250", 
-                                                   "inc200225", "inc175200", "inc150175", "inc125150", "inc100125", "inc75100", "inc5075", "inc2550", "inc025")),
+         mutate(income = factor(income, levels = c("inc025", "inc2550", "inc5075", "inc75100", "inc100125", "inc125150", "inc150175","inc175200", "inc200225", 
+                                                   "inc225250", "inc250275", "inc275300", "inc300325", "inc325350", "inc350375", "inc375400", "inc400+")),
                 #ugh
                 education = ifelse(education == "noHS", "No High School",
                               ifelse(education == "HS", "High School",
@@ -53,9 +52,7 @@ icons <- rbind(iconsLoSub, iconsHi) %>%
                 education = factor(education, levels = c("No High School", "High School", "Some College", "Bachelor's Degree","Graduate Degree")),
                 familyType = factor(familyType, levels = c("Single", "Single with Dependent(s)", "Couple")),
                 race = factor(race, levels = c("White","Latino", "Black", "Other")), 
-                gender = factor(gender, levels = c("Woman","Man"))) %>%
-                
-                arrange(income, gender, familyType, education)
+                gender = factor(gender, levels = c("Woman","Man"))) 
 
 #completed data file
 write.csv(icons,"iconsFilled.csv", row.names=FALSE)
